1b7380fe522768dcef573b1ed4c5b80793783ee2
sunwucode <wukongkhan8@gmail.com>
2019-04-02 15:16:34 +0200

    created column class with Abi

--- module1.js
+++ module1.js
@@ -5,6 +5,17 @@ canvas.height = canvas.scrollHeight;
 
 var ctx = canvas.getContext("2d");
 
+class column {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
 // the ball1
 ctx.fillStyle = "black";
 ctx.beginPath();
@@ -18,8 +29,11 @@ ctx.fillRect(220, 390, 100, 100);
 ctx.fillRect(330, 390, 100, 100);
 ctx.fillRect(330, 280, 100, 100);
 
-//column
+//column Array
 ctx.fillRect(600, 280, 100, 310);
 
 // moving plateforme
 ctx.fillRect(800, 280, 200, 50);
+
+var column2 = new column(1500, 100, 100, 100);
+column2.draw();
