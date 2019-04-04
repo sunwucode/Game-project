52afaf8ad633bf3f45ced293f69687d2f5d049f1
sunwucode <wukongkhan8@gmail.com>
2019-04-04 22:52:48 +0200

    Things has gone a little south while trying to implement collisions, code has been checked by Gui, I just corrected the top limit

--- module1.js
+++ module1.js
@@ -1,6 +1,7 @@
 // General Setting
 
 var currentColor = "black";
+var gravity = 1;
 
 var myGameArea = {
   canvas: document.getElementById("canvas"),
@@ -31,21 +32,12 @@ var myGameArea = {
         case 88: // key x
           currentColor = currentColor === "black" ? "white" : "black";
           // ternaire, très utile pour des attributions.
-          // containedObjects.map(function(item) {
-          //   if (item.color === "white") {
-          //     color = "black";
-          //   }
-          //   if (item.color === "black") {
-          //     color = "white";
-          //   }
-          //   console.log(item);
-          // });
+
           break;
       }
     };
   },
   start: function() {
-    console.log(this);
     this.setCanvas();
     this.listenKeyboard();
   },
@@ -54,42 +46,38 @@ var myGameArea = {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 };
-
-// update() {
-
+//calls all the obstacles from the Array
 function drawObstacles() {
-  myGameArea.ctx.fillRect(220, 500, 100, 100);
-  myGameArea.ctx.fillRect(330, 500, 100, 100);
-  myGameArea.ctx.fillRect(220, 390, 100, 100);
-  myGameArea.ctx.fillRect(330, 390, 100, 100);
-  myGameArea.ctx.fillRect(330, 280, 100, 100);
-
-  // moving plateforme
-  myGameArea.ctx.fillRect(800, 280, 200, 50);
+  player1.draw();
+  for (let i = 0; i < myGameArea.containedObjects.length; i++) {
+    myGameArea.containedObjects[i].draw();
+  }
 }
 
 function updateGameArea() {
   myGameArea.clear();
 
   drawObstacles();
+
   player1.newPos();
   player1.boundaries();
   player1.draw();
+  player1.speedY += gravity;
+  rectangleCollision(player1, myGameArea.containedObjects);
 }
 // Definition of the Player object / Ball class
 
 class Ball {
   constructor(x, y, radius, color) {
-    this.radius = radius;
-    this.color = color;
     this.x = x;
     this.y = y;
-    // new speed properties
+    this.radius = radius;
+    this.color = color;
     this.speedX = 0;
     this.speedY = 0;
-    isblocked: false;
+    this.isBlocked = false;
   }
-  draw(context) {
+  draw() {
     //myGameArea.ctx.fillStyle = this.color;
     myGameArea.ctx.fillStyle = currentColor;
     myGameArea.ctx.beginPath();
@@ -98,24 +86,78 @@ class Ball {
     myGameArea.ctx.fill();
   }
   newPos() {
-    this.x += this.speedX;
-    this.y += this.speedY;
+    function checkXvalid(ballX, balRadius, obstX, obstWidth) {
+      // console.log("obstX", obstX);
+      return ballX + balRadius >= obstX;
+    }
+    // console.log("thisX", this.x);
+    for (let i = 0; i < myGameArea.containedObjects.length; i++) {
+      // console.log(myGameArea.containedObjects[i]);
+
+      if (
+        checkXvalid(
+          this.x,
+          this.radius,
+          myGameArea.containedObjects[i].x,
+          myGameArea.containedObjects[i].width
+        )
+      ) {
+        this.y += this.speedY;
+      } else {
+        this.x += this.speedX;
+        this.y += this.speedY;
+      }
+    }
+    //bally + balRadius >= obstY
+    // function checkYvalid(bally, balRadius, obstY, obstHeight) {
+    //   return bally + balRadius <= obstY || bally + balRadius >= obstY;
+    // }
+    // console.log(checkYvalid());
+    // for (let i = 0; i < myGameArea.containedObjects.length; i++) {
+    //   if (
+    //     checkYvalid(
+    //       this.y,
+    //       this.radius,
+    //       myGameArea.containedObjects[i].y,
+    //       myGameArea.containedObjects[i].height
+    //     )
+    //   ) {
+    //     this.x += this.speedX;
+    //   } else {
+    //     this.x += this.speedX;
+    //     this.y += this.speedY;
+    //   }
+    // }
   }
+
   boundaries() {
-    if (this.y + this.speedY > canvas.height || this.y + this.speedY < 0) {
-      this.speedY = 0;
+    if (this.y + this.radius >= canvas.height) {
+      this.y = canvas.height - this.radius;
+      this.boundaries.speedY = 0;
+      gravity = 0;
+    } else if (this.y - this.radius < 0) {
+      this.y = this.radius;
+      gravity = 0;
     }
-    if (this.x + this.speedX > canvas.width || this.x + this.speedX < 0) {
-      this.speedX = 0;
-    }
+    // if (
+    //   this.y + this.speedY > canvas.height ||
+    //   this.y + this.speedY < this.radius
+    // ) {
+    //   this.y = 0;
+    // }
+    // if (
+    //   this.x + this.speedX > canvas.width ||
+    //   this.x + this.speedX < this.radius
+    // ) {
+    //   this.speedX = 0;
+    // }
   }
 }
+
 myGameArea.start();
 let player1 = new Ball(50, 550, 50, "black");
-myGameArea.containedObjects.push(player1);
+// myGameArea.containedObjects.push(player1);
 player1.draw(myGameArea.ctx);
-
-//Keyboard controls
 
 // key up function - to apply ?
 // document.onkeyup = function(event) {
@@ -125,12 +167,15 @@ player1.draw(myGameArea.ctx);
 
 // obstacles /////////////////////////////////////////////
 class Shapes {
-  constructor(x, y, width, height, color) {
+  constructor(x, y, width, height, color, speedX, speedY) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.color = color;
+    this.speedX = speedX || 0;
+    this.speedY = speedY || 0;
+    this.draw();
   }
   draw() {
     myGameArea.ctx.fillStyle = currentColor;
@@ -142,62 +187,31 @@ class Column extends Shapes {
   constructor(x, y, width, height, color) {
     super(x, y, width, height, color);
   }
-  // draw() {
-  //   myGameArea.ctx.fillStyle = currentColor;
-  //   myGameArea.ctx.fillRect(this.x, this.y, this.width, this.height);
-  // } not necessary
 }
-//first obstacle
-drawObstacles();
 
-//column Array
-// ctx.fillRect(600, 280, 100, 310);
-// var column2 = new column(1500, 100, 100, 100);
-// column2.draw();
-// var columns = [new Column(1500, 100, 100, 100), new Column(600, 280, 100, 310)];
-myGameArea.containedObjects.push(new Column(1500, 100, 100, 100));
-myGameArea.containedObjects.push(new Column(600, 280, 100, 310));
 class movingPlateform {}
 
-// drawLoop();
-
-// function drawLoop() {
-//   ctx.clearRect(player1.x, player1.y, 50, 0, Math.PI * 2);
-//   drawPlayer1();
-
-//   requestAnimationFrame(function() {
-//     drawLoop();
-//   });
-// }
-// setInterval(function() {
-//   update();
-//   player1.draw(ctx);
-// }, 100);
-
-// function update() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-// }
-
-// window.onclick = function() {
-//   currentColor = "yellow";
-// };
-
-// Collision
+// Collision (of Abi's flappy bird)
 // -----------------------------------------------------------------------------
-// function rectangleCollision(player1, rectB) {
-//   return (
-//     player1.y + player1.height >= rectB.y &&
-//     player1.y <= rectB.y + rectB.height &&
-//     player1.x + player1.width >= rectB.x &&
-//     player1.x <= rectB.x + rectB.width
-//   );
-// }
+function rectangleCollision(player1, rectB) {
+  // console.log(player1, rectB.slice(20));
+  return (
+    player1.y + player1.height >= rectB.y &&
+    player1.y <= rectB.y + rectB.height &&
+    player1.x + player1.width >= rectB.x &&
+    player1.x <= rectB.x + rectB.width
+  );
+}
 
-// function checkCrashes() {
-//   allPipes.forEach(function(onePipe) {
-//     if (rectangleCollision(celine, onePipe)) {
-//       celine.isCrashed = true;
-//       onePipe.isCrashed = true;
-//     }
-//   });
-// }
+function initObstacles() {
+  myGameArea.containedObjects.push(new Column(220, 500, 100, 100));
+  // myGameArea.containedObjects.push(new Column(330, 500, 100, 100));
+  // myGameArea.containedObjects.push(new Column(220, 390, 100, 100));
+  // myGameArea.containedObjects.push(new Column(330, 390, 100, 100));
+  // myGameArea.containedObjects.push(new Column(330, 280, 100, 100));
+  // myGameArea.containedObjects.push(new Column(1500, 100, 100, 100));
+  // myGameArea.containedObjects.push(new Column(600, 280, 100, 310));
+  // myGameArea.containedObjects.push(new Column(800, 280, 200, 50));
+}
+
+initObstacles();
